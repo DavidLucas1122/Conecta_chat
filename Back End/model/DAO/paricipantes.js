40155f591a95816fbe82c36ba17a9916e922a9e7
unknown <DavidLucas1122>
2026-08-01 01:10:46 -0300

    Edição de retorno em participante

--- Back End/model/DAO/paricipantes.js
+++ Back End/model/DAO/paricipantes.js
@@ -5,17 +5,17 @@ const db = knex(knexConfig.development);
 
 const getConversationByIdUser = async (idUsuario) => {
   try {
-    const result = await db("tbl_participantes")
-      .select("tbl_conversa.*")
-      .join(
-        "tbl_conversa",
-        "tbl_participantes.id_conversa",
-        "tbl_conversa.id_conversa",
-      )
-      .where("tbl_participantes.id_usuario", idUsuario);
+    const result = await db("tbl_participantes as p1")
+      .select("c.id_conversa", "u.id_usuario", "u.nome", "u.email", "u.imagem")
+      .join("tbl_participantes as p2", "p1.id_conversa", "p2.id_conversa")
+      .join("tbl_usuario as u", "p2.id_usuario", "u.id_usuario")
+      .join("tbl_conversa as c", "c.id_conversa", "p1.id_conversa")
+      .where("p1.id_usuario", idUsuario)
+      .whereNot("p2.id_usuario", idUsuario);
 
     return result;
   } catch (error) {
+    console.log(error);
     return false;
   }
 };
